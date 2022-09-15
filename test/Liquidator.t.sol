02fce001f58065821ba1c918b7cd00fe68c17ef7
danilo neves cruz <cruzdanilo@gmail.com>
2022-09-15 15:21:48 -0300

    🧑‍💻 contracts: improve forge integration

--- test/Liquidator.t.sol
+++ test/Liquidator.t.sol
@@ -6,7 +6,7 @@ import { Market } from "@exactly-protocol/protocol/contracts/Market.sol";
 import { Auditor } from "@exactly-protocol/protocol/contracts/Auditor.sol";
 import { MockPriceFeed } from "@exactly-protocol/protocol/contracts/mocks/MockPriceFeed.sol";
 import { ExactlyOracle, AggregatorV2V3Interface } from "@exactly-protocol/protocol/contracts/ExactlyOracle.sol";
-import { Liquidator, IMarket, ERC20 } from "../contracts/Liquidator.sol";
+import { Liquidator, IMarket, ERC20, PoolAddress } from "../contracts/Liquidator.sol";
 
 contract LiquidatorTest is Test {
   using stdJson for string;
@@ -19,21 +19,34 @@ contract LiquidatorTest is Test {
   Market internal marketDAI;
   Auditor internal auditor;
   address internal timelock;
+  ERC20 internal usdc;
+  ERC20 internal dai;
 
   function setUp() public {
     liquidator = new Liquidator(getAddress("UniswapV3Factory", ""));
 
-    marketUSDC = Market(getAddress("MarketUSDC", "node_modules/@exactly-protocol/protocol/"));
-    marketDAI = Market(getAddress("MarketDAI", "node_modules/@exactly-protocol/protocol/"));
+    dai = ERC20(getAddress("DAI", "node_modules/@exactly-protocol/protocol/"));
+    usdc = ERC20(getAddress("USDC", "node_modules/@exactly-protocol/protocol/"));
+    oracle = ExactlyOracle(getAddress("ExactlyOracle", "node_modules/@exactly-protocol/protocol/"));
+    oracle = ExactlyOracle(getAddress("ExactlyOracle", "node_modules/@exactly-protocol/protocol/"));
     auditor = Auditor(getAddress("Auditor", "node_modules/@exactly-protocol/protocol/"));
     timelock = getAddress("TimelockController", "node_modules/@exactly-protocol/protocol/");
-    oracle = ExactlyOracle(getAddress("ExactlyOracle", "node_modules/@exactly-protocol/protocol/"));
+    marketDAI = Market(getAddress("MarketDAI", "node_modules/@exactly-protocol/protocol/"));
+    marketUSDC = Market(getAddress("MarketUSDC", "node_modules/@exactly-protocol/protocol/"));
+
+    vm.label(ALICE, "alice");
+    if (address(dai) != address(0)) {
+      vm.label(
+        PoolAddress.computeAddress(liquidator.factory(), PoolAddress.getPoolKey(address(dai), address(usdc), 500)),
+        "DAI/USDC/500"
+      );
+    }
   }
 
   function testMultiMarketLiquidation() external {
-    deal(address(marketUSDC.asset()), ALICE, 100_000e6);
+    deal(address(usdc), ALICE, 100_000e6);
     vm.startPrank(ALICE);
-    marketUSDC.asset().approve(address(marketUSDC), type(uint256).max);
+    usdc.approve(address(marketUSDC), type(uint256).max);
     marketUSDC.deposit(100_000e6, ALICE);
     auditor.enterMarket(marketUSDC);
     marketDAI.borrow(70_000 ether, ALICE, ALICE);
@@ -43,22 +56,22 @@ contract LiquidatorTest is Test {
     oracle.setPriceFeed(marketDAI, AggregatorV2V3Interface(address(new MockPriceFeed(1.4e8))));
     vm.stopPrank();
 
-    uint256 balanceBefore = marketUSDC.asset().balanceOf(address(liquidator));
+    uint256 balanceBefore = usdc.balanceOf(address(liquidator));
     liquidator.liquidate(
       IMarket(address(marketDAI)),
       IMarket(address(marketUSDC)),
       ALICE,
       67_300 ether,
-      address(marketUSDC.asset()),
+      address(usdc),
       500
     );
-    assertGt(marketUSDC.asset().balanceOf(address(liquidator)), balanceBefore);
+    assertGt(usdc.balanceOf(address(liquidator)), balanceBefore);
   }
 
   function testSingleMarketLiquidation() external {
-    deal(address(marketUSDC.asset()), ALICE, 100_000e6);
+    deal(address(usdc), ALICE, 100_000e6);
     vm.startPrank(ALICE);
-    marketUSDC.asset().approve(address(marketUSDC), type(uint256).max);
+    usdc.approve(address(marketUSDC), type(uint256).max);
     marketUSDC.deposit(100_000e6, ALICE);
     auditor.enterMarket(marketUSDC);
     marketDAI.borrow(70_000 ether, ALICE, ALICE);
@@ -69,20 +82,19 @@ contract LiquidatorTest is Test {
     oracle.setPriceFeed(marketDAI, AggregatorV2V3Interface(address(new MockPriceFeed(1.4e8))));
     vm.stopPrank();
 
-    liquidator.liquidate(
-      IMarket(address(marketUSDC)),
-      IMarket(address(marketUSDC)),
-      ALICE,
-      2_000e6,
-      address(marketDAI.asset()),
-      500
-    );
-    assertGt(marketUSDC.asset().balanceOf(address(liquidator)), 0);
+    liquidator.liquidate(IMarket(address(marketUSDC)), IMarket(address(marketUSDC)), ALICE, 2_000e6, address(dai), 500);
+    assertGt(usdc.balanceOf(address(liquidator)), 0);
   }
 
-  function getAddress(string memory name, string memory base) internal returns (address) {
+  function getAddress(string memory name, string memory base) internal returns (address addr) {
+    if (block.chainid == 31337) return address(0);
+
     string memory network;
-    if (block.chainid == 4) network = "rinkeby";
-    return vm.readFile(string.concat(base, "deployments/", network, "/", name, ".json")).readAddress(".address");
+    if (block.chainid == 1) network = "mainnet";
+    else if (block.chainid == 4) network = "rinkeby";
+    else if (block.chainid == 5) network = "goerli";
+
+    addr = vm.readFile(string.concat(base, "deployments/", network, "/", name, ".json")).readAddress(".address");
+    vm.label(addr, name);
   }
 }
