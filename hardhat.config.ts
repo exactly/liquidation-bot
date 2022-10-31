49a0857e71e2500ed31450d59e38e250efb6df25
danilo neves cruz <cruzdanilo@gmail.com>
2022-10-31 13:41:34 -0300

    🔧 mainnet: setup network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,6 +8,10 @@ import type { HardhatUserConfig } from "hardhat/types";
 export default {
   solidity: { version: "0.8.17", settings: { optimizer: { enabled: true, runs: 6_666_666 } } },
   networks: {
+    mainnet: {
+      url: env.MAINNET_NODE ?? "https://mainnet.infura.io/",
+      ...(env.MNEMONIC && { accounts: { mnemonic: env.MNEMONIC } }),
+    },
     goerli: {
       url: env.GOERLI_NODE ?? "https://goerli.infura.io/",
       ...(env.MNEMONIC && { accounts: { mnemonic: env.MNEMONIC } }),
@@ -16,6 +20,10 @@ export default {
   typechain: { outDir: "types" },
   namedAccounts: {
     deployer: { default: 0 },
-    owner: { default: 0, goerli: "0x1801f5EAeAbA3fD02cBF4b7ED1A7b58AD84C0705" },
+    owner: {
+      default: 0,
+      mainnet: "0x382d89aa156C473Fdb1c9565dF309e80e8fA4437",
+      goerli: "0x1801f5EAeAbA3fD02cBF4b7ED1A7b58AD84C0705",
+    },
   },
 } as HardhatUserConfig;
