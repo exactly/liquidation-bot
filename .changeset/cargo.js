543966d142fab7487e1de1570b406d03929e776f
danilo neves cruz <cruzdanilo@gmail.com>
2023-04-05 21:30:32 -0300

    🔨 setup changesets

---
+++ .changeset/cargo.js
@@ -0,0 +1,6 @@
+const { readFileSync, writeFileSync } = require("fs");
+const { parse, stringify, basic } = require("@ltd/j-toml");
+const { version } = require("../package.json");
+const cargo = parse(readFileSync("Cargo.toml"), { x: { literal: true } });
+cargo.package.version = basic(version);
+writeFileSync("Cargo.toml", stringify(cargo, { newline: "\n", newlineAround: "section" }).trimStart());
