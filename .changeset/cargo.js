dbbb1b48de577fa053a138a8d92382cf8a8f73b9
danilo neves cruz <cruzdanilo@gmail.com>
2023-05-23 23:54:18 +0200

    👷 github: update cargo and npm lock after version

--- .changeset/cargo.js
+++ .changeset/cargo.js
@@ -1,6 +1,8 @@
+const { exec } = require("child_process");
 const { readFileSync, writeFileSync } = require("fs");
 const { parse, stringify, basic } = require("@ltd/j-toml");
 const { version } = require("../package.json");
 const cargo = parse(readFileSync("Cargo.toml"), { x: { literal: true } });
 cargo.package.version = basic(version);
 writeFileSync("Cargo.toml", stringify(cargo, { newline: "\n", newlineAround: "section" }).trimStart());
+exec("cargo metadata");
